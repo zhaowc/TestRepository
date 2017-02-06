2cabfc1d3f61ddab4857a409af37cfc51b99b3d5
zhaowenchao <wenchao.zhao@sachsen.cc>
2017-02-06 20:05:32 +0800

    second commit

--- app/src/main/java/com/example/zhaowenchao/myapplication/MainActivity.java
+++ app/src/main/java/com/example/zhaowenchao/myapplication/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mTv = (TextView) findViewById(R.id.tv);
+        mTv.append("good");
     }
 }
 
